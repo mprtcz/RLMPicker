cc17f70bd66749029b87f49eae52e59290310809
mprtcz <michalpartacz@gmail.com>
2021-07-10 08:55:02 +0200

    Added initial skeleton of the app and the files required.

---
+++ src/data/consts.js
@@ -0,0 +1,15 @@
+export const EpisodeSeries = {
+    BestOfTheWorst: 'BestOfTheWorst'
+}
+
+export const Members = {
+    JAY: 'Jay',
+    MIKE: 'Mike',
+    RICH: 'Rich Evans'
+}
+
+export const SubSeries = {
+    DEFAULT: '',
+    WHEEL_OF_THE_WORST = 'Wheel of the worst',
+    WHEEL_OF_MISFORTUNE = 'Wheel of misfortune'
+}
