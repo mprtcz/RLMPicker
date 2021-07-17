409577ad60eac6cea2feae41f3cdd7b98b9ec6f4
mprtcz <azet89"gmail.com>
2021-07-17 16:50:50 +0200

    Moved filters to a openable accordion

---
+++ src/components/Filters.js
@@ -0,0 +1,48 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Accordion from "@material-ui/core/Accordion";
+import AccordionSummary from "@material-ui/core/AccordionSummary";
+import AccordionDetails from "@material-ui/core/AccordionDetails";
+import Typography from "@material-ui/core/Typography";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import MatMultiselect from "./MatMultiselect";
+import ItemSelect from "./Select";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: "100%",
+  },
+  accordion: {
+    width: "90%",
+  },
+  heading: {
+    fontSize: theme.typography.pxToRem(15),
+    fontWeight: theme.typography.fontWeightRegular,
+  },
+}));
+
+const Filters = (props) => {
+  const classes = useStyles();
+  const { episodesData, onSelect } = props;
+  return (
+    <Accordion className={classes.accordion}>
+      <AccordionSummary
+        expandIcon={<ExpandMoreIcon />}
+        aria-controls="panel1a-content"
+        id="panel1a-header"
+      >
+        <Typography className={classes.heading}>Filters</Typography>
+      </AccordionSummary>
+      <AccordionDetails className="filters-container">
+        <MatMultiselect
+          data={episodesData}
+          filterType="members"
+          onSelect={(e) => onSelect(e)}
+        />
+        <ItemSelect items={episodesData} onSelect={(e) => onSelect(e)} />
+      </AccordionDetails>
+    </Accordion>
+  );
+};
+
+export default Filters;
--- src/components/Page.js
+++ src/components/Page.js
@@ -2,6 +2,7 @@ import { makeStyles } from "@material-ui/core";
 import { data } from "data/new-data";
 import { searchData } from "functions/searchData";
 import React, { useEffect, useState } from "react";
+import Filters from "./Filters";
 import MatMultiselect from "./MatMultiselect";
 import Results from "./Results";
 import ItemSelect from "./Select";
@@ -9,7 +10,7 @@ import ItemSelect from "./Select";
 const useStyles = makeStyles({
   content: {
     flexDirection: "row",
-    flexWrap: "wrap-reverse",
+    flexWrap: "wrap",
     display: "flex",
     justifyContent: "center",
   },
@@ -50,15 +51,11 @@ const Page = () => {
       </div>
 
       <div className={classes.content}>
+        <Filters
+          episodesData={episodesData}
+          onSelect={(e) => setResults(e)}
+        ></Filters>
         <div className={classes.results}>
-          <div className="filters-container">
-            <MatMultiselect
-              data={episodesData}
-              filterType="members"
-              onSelect={(e) => setResults(e)}
-            />
-            <ItemSelect items={episodesData} onSelect={(e) => setResults(e)} />
-          </div>
           <Results results={resultsEpsodes} />
         </div>
       </div>
