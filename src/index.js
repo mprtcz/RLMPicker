fdb970e625e552177f69d03f2c0d2e06808eb814
mprtcz <azet89"gmail.com>
2021-12-19 22:16:34 +0100

    Added GA the vanilla way.

--- src/index.js
+++ src/index.js
@@ -1,14 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+
+const injectGA = () => {
+  if (typeof window == "undefined") {
+    return;
+  }
+  window.dataLayer = window.dataLayer || [];
+  function gtag() {
+    window.dataLayer.push(arguments);
+  }
+  gtag("js", new Date());
+
+  gtag("config", "YOUR_TRACKING_ID");
+};
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    {/* Global site tag (gtag.js) - Google Analytics */}
+    <script
+      async
+      src="https://www.googletagmanager.com/gtag/js?id=YOUR_TRACKING_ID"
+    />
+    <script>{injectGA()}</script>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
