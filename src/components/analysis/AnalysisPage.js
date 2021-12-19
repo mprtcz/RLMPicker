76e6ec9e8e1e1b2ee2c8471f984c793176c0f0fb
mprtcz <michalpartacz@gmail.com>
2021-12-19 14:55:41 +0100

    Added pie chart for the appaearance of members by count

--- src/components/analysis/AnalysisPage.js
+++ src/components/analysis/AnalysisPage.js
@@ -1,9 +1,61 @@
 import React from "react";
+import Paper from "@material-ui/core/Paper";
+import {
+  Chart,
+  Legend,
+  PieSeries,
+  Tooltip,
+} from "@devexpress/dx-react-chart-material-ui";
+import { HoverState, EventTracker } from "@devexpress/dx-react-chart";
+import { useVideoData, VideoDataContext } from "contexts/VideosDataContext";
 
 const AnalysisPage = () => {
+  const videoData = useVideoData();
+
+  const getPieChartData = () => {
+    const result = videoData.videos
+      .map((videoData) => videoData.members)
+      .flat()
+      .reduce((map, word) => {
+        if (word in map) {
+          map[word]++;
+        } else {
+          map[word] = 1;
+        }
+        return map;
+      }, {});
+
+    return Object.keys(result)
+      .map((key) => {
+        const name = key;
+        const count = result[key];
+        return { name, count };
+      })
+      .sort((a, b) => b.count - a.count);
+  };
+
   return (
     <div>
-      <h1>AnalysisPage</h1>
+      <VideoDataContext.Consumer>
+        {() => (
+          <span>
+            <Paper>
+              <h1>Core member by appearance</h1>
+              <Chart data={getPieChartData()}>
+                <PieSeries
+                  valueField="count"
+                  argumentField="name"
+                  name="name"
+                />
+                <Legend />
+                <EventTracker />
+                <HoverState />
+                <Tooltip />
+              </Chart>
+            </Paper>
+          </span>
+        )}
+      </VideoDataContext.Consumer>
     </div>
   );
 };
